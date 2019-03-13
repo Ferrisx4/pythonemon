d579e9e8815dda2d418b8422d9d4b2be282ae5fa
Ferrisx4 <ferrisx4@gmail.com>
2019-03-13 15:38:44 -0400

    Restructure, put basic test functions

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from pkmn_classes import *
 from combat import *
 
-def mainLoop():
+
+def test_pokemon_creation():
     pikachu = pokemon('Pikachu', 50)
     pikachu.printParty()
     
     foe = pokemon('Goldeen', 30)
     foe.printParty()
-    
+
+def test_type_effectiveness():
     print 'Electric vs Goldeen: '
     print calculate_effectiveness('Electric','Water',None)
     print 'Electric vs Diglett: '
@@ -16,5 +18,20 @@ def mainLoop():
     print calculate_effectiveness('Electric','Water','Flying')
     print 'Electric vs Bellsprout: '
     print calculate_effectiveness('Electric','Grass',None)
+
+def test_move_import():
+    from ROM import moves
+    print'Imported ' + str(len(moves)) + ' moves.'
+
+def mainLoop():
+    test_pokemon_creation()
+
+    print '===================='
+
+    test_type_effectiveness()
+    
+    print '===================='
+
+    test_move_import()    
         
 mainLoop()
