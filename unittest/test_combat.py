deb5389aeca5f883a9f7836a4cceb7d11e5b8e12
Ferrisx4 <ferrisx4@gmail.com>
2019-09-12 10:24:27 -0400

    add test_combat.py

---
+++ unittest/test_combat.py
@@ -0,0 +1,9 @@
+import unittest
+from ..combat import calculate_effectiveness
+
+class TestCombat(unittest.TestCase):
+    #def test_calculate_damage(self):
+    #    self.assertEqual(calculate_damage(50, 50, 40, 30, 'Rock'), 2, "Should be 2")
+
+    def test_calculate_effectiveness(self):
+        self.assertEqual(calculate_effectiveness('Electric','Water','Flying'),2.0,"Should be 2")
