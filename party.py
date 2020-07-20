46960e3e3855bbde7fb3f4e129e4f54ed76ecb1f
Ferrisx4 <ferrisx4@gmail.com>
2020-07-20 15:26:40 -0400

    init

--- party.py
+++ party.py
@@ -1,3 +1,15 @@
+class party:
+    def __init__(self,p1,p2,p3,p4,p5,p6):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+        self.p4 = p4
+        self.p5 = p5
+        self.p6 = p6
+
+    @classmethod
+    def print_party
+
 def getParty(member_no):
     # Open the party db/file
     
