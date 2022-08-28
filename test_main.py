c04bfba991892334a3d6b14c4099550dc8962c99
Ferrisx4 <ferrisx4@gmail.com>
2022-08-27 22:54:09 -0400

    more examples, change function args

--- test_main.py
+++ test_main.py
@@ -4,11 +4,11 @@ from combat import combat
 from party import *
 
 # This is a series of functions to test the actual functions of the program
-
 def test_pokemon_creation():
     from ROM import pokemon_dict
     # Get an instance of a Pokémon
-    pokemon_1_instance = pokemon_instance('Ivysaur', 50)
+    pokemon_1_instance = pokemon_instance('Ivysaur','d',50)
+
     # Get the data for that Pokémon
     pokemon_1_data=pokemon_dict[pokemon_1_instance.species]
 
@@ -19,7 +19,7 @@ def test_pokemon_creation():
     print(repr(pokemon_1_data))
     print(pokemon_1_data)
     print(pokemon_1_data.get_types())
-    foe = pokemon_instance('Goldeen', 30)
+    foe = pokemon_instance('Goldeen','', 30)
 
     # Put the pokemon_instance into a party, print it.
     my_party = party(pokemon_1_instance,None,None,None,None,None)
@@ -34,9 +34,14 @@ def test_type_effectiveness():
     print(combat.calculate_effectiveness('Electric','Water','Flying'))
     print('Electric vs Bellsprout: ')
     print(combat.calculate_effectiveness('Electric','Grass',None))
+    print('Ground vs a real Pikachu: ')
+    from ROM import pokemon_dict
+    print(pokemon_dict['Pikachu'])
+
     #combat.do_move(3,scene)
 def test_move_import():
     from ROM import moves
+    #print('size of moves: ' + sys.getsizeof(moves))
     print('Imported ' + str(len(moves)) + ' moves.')
     print(moves[0].name + ' has ' + moves[0].accuracy + '% accuracy.')
 
