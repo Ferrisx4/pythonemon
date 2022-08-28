b07a9975ab53fd1c690085b3a1f84587944b1172
Ferrisx4 <ferrisx4@gmail.com>
2022-08-27 22:51:40 -0400

    calculate_damage wip

--- combat.py
+++ combat.py
@@ -1,4 +1,6 @@
 # -*- coding: latin-1 -*-
+
+import random
 class combat:
     def __init__(self,party1,party2):
         self.p1 = party1
@@ -18,7 +20,7 @@ class combat:
             #print "Unknown type or type effectiveness, assuming 1x"
             pass
             
-        # Defender Type 1
+        # Defender Type 2
         try:
             effectiveness = effectiveness * Ef1[move_type][defender_type2]
         except KeyError:
@@ -29,11 +31,23 @@ class combat:
         return effectiveness
 
     @staticmethod
-    def calculate_damage(level, power, attack, defense, movetype):
+    def calculate_damage(level, power, attack, defense, movetype, target_type1, target_type2):
         # Damage formula: (basically Generation 1 but using type split from generation 4+)
-        # damage =   (((((2 * level) / 5) + 2) * (power * (attack/defense)) / 50) + 2 ) * modifier
+        # damage =   (((((2 * level * Critical) / 5) + 2) * (power * (attack/defense)) / 50) + 2 ) * modifier
+        # level:  attacker's level
+        # Critical: 1 normal, 2 if critical
+        # 
         # modifier = rand(0.85,1) * STAB * type_effectiveness()
-        return 0
+        #return 0
+
+        # Save time, is the move effective?
+        effectiveness = combat.calculate_effectiveness(movetype, target_type1, target_type2)
+        if effectiveness == 0:
+            return 0
+        # Looks like the move is not useless, continue calculating damage
+        modifier = random.uniform(0.85,1.0)
+        damage = ((((( 2 * level ) / 5 ) + 2) * ( power * (attack/defense)) / 50) + 2) * modifier
+        return damage
 
     # 
     # Takes into consideration whether the move still has PP, is not disabled, and is not blocked
